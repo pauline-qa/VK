b06c64effe732c62df0285a54b87685e198aaeac
Polina Novikova <novikovapauline@gmail.com>
2021-09-09 21:56:19 +0300

    After docs review

--- src/main/java/domain/Pagevknetology.java
+++ src/main/java/domain/Pagevknetology.java
@@ -9,10 +9,11 @@ public class VkPost {
     private int date;
     private String imageUrl;
     private String text;
-    private boolean myLike;
-    private int numberOfLikes;
-    private int numberOfViews;
-    private int numberOfReposts;
+    private int createdBy;
+    private int isPinned;
+    private int markedAsAds;
+    private boolean isFavorite;
+    private int postponedId;
 
     // + get/set на все поля
 }
@@ -34,4 +35,44 @@ public class CommentsInfo {
 
 public class InnerThreeDotMenu { // меню типа объект, под такой типа объекта - отдельный класс
     // здесь должно быть 2 метода Пожаловаться и Сохранить в закладках
+}
+
+public class CopyRight {
+    private int id;
+    private String link;
+    private String name;
+    private String type;
+    // + get/set на все поля
+}
+
+public class Like {
+    private int count;
+    private int userLikes;
+    private int canLike;
+    private int canPublish;
+    // + get/set на все поля
+}
+
+public class Repost {
+    private int count;
+    // + get/set на все поля
+}
+
+public class Views {
+    private int count;
+    // + get/set на все поля
+}
+
+public class Geo {
+    private String type;
+    private String coordinates;
+    // + get/set на все поля
+}
+
+public class Donut {
+    private boolean isDonut;
+    private int paidDuration;
+    private boolean canpublishFeeCopy;
+    private String editMode;
+    // + get/set на все поля
 }
